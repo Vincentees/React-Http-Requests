be7bc4ebb57281f08c00d49f83f229575f249c75
kipngetichvincent.k@gmail.com <kipngetichvincent.k@gmail.com>
2021-01-17 01:15:00 +0300

    Set a default Global configuration for axios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
+axios.defaults.baseURL='https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization']='AUTH TOKEN';
+axios.defaults.headers.post['content-Type']='application/json';
 
 axios.interceptors.request.use(request=>{
     console.log(request);
