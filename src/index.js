2603ef50275c11acbfaf848fb35efb475a80b4df
kipngetichvincent.k@gmail.com <kipngetichvincent.k@gmail.com>
2021-01-16 21:47:46 +0300

    Added interceptors to execute code globally

--- src/index.js
+++ src/index.js
@@ -12,12 +12,15 @@ axios.interceptors.request.use(request=>{
     console.log(error);
     return Promise.reject(error);
 });
+
 axios.interceptors.response.use(response=>{
     console.log(response);
+    return response;
 }, error=>{
     console.log(error);
     return Promise.reject(error);
-})
+});
+
 
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
